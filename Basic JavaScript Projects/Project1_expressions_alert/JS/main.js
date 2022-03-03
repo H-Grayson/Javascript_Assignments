b1a4d61d273242c40904d8392f3c5ed0753c0ee0
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-03-02 20:52:32 -0700

    Javascript Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("Boo!"); // Says boo as an alert
+
+document.write("You're doing great chief!"); 
+
+var A = "I have Pets!"; // Giving variable A a string
+
+var B = "2 dogs and 2 cats" // Giving variable B a string
+
+
+var Brown_Dog = "Doug", Black_Dog = "Lilly", // Pet names
+    White_Cat = "Shiba", Black_Cat = "Felix"; 
+
+var z = (A + B); // couldn't get this to work idk..
+
+A.fontcolor(red); // assigning A the fontcolor red
+

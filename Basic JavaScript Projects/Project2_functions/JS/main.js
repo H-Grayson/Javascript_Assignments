0ac9b21abeabd8977814002887f7cdb64ef5a263
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-03-11 15:16:38 -0700

    corrections

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,12 +1,12 @@
 function My_First_Function() {
-    var str= "This is the button text!";
-    document.getElementById("Button_Text").innerHTML = str;
-    var str1= "This is a paragraph element!";
-    document.getElementById("Paragraph_Text").innerHTML = str1;
+    var str= "This is the button text!"; //Assigning str text
+    document.getElementById("Button_Text").innerHTML = str; //Giving str an id
+    var str1= "This is a paragraph element!"; //Assigning str1 text
+    document.getElementById("Paragraph_Text").innerHTML = str1; //Giving str1 an id
 }
 
 function myFunction() {
-    var sentence = "I am learning";
-    sentence+= " a lot from this course!";
-    document.getElementById("Concatenate_String").innerHTML = sentence;
+    var sentence = "I am learning"; // Assigning sentence text
+    sentence+= " a lot from this course!"; // Concatenating with the var sentence
+    document.getElementById("Concatenate_String").innerHTML = sentence; //Giving sentence an id
 }
